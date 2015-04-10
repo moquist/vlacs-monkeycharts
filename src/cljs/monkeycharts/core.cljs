cef174d61b500b885d94e528e84fa225fd405e9f
Matt Oquist <moquist@majen.net>
2015-04-09 22:50:36 -0400

    Add initial pieces of client to fetch data.
    The first chart to be done will be for school size. No chart yet, but we can get
    the data to the client and render it as a few thousand <p> elements.
    
    It is a start.

--- src/cljs/monkeycharts/core.cljs
+++ src/cljs/monkeycharts/core.cljs
@@ -1,20 +1,36 @@
-(ns ^:figwheel-always monkeycharts.core
+(ns ^:figwheel-always
+  monkeycharts.core
     (:require[om.core :as om :include-macros true]
-              [om.dom :as dom :include-macros true]))
+              [om.dom :as dom :include-macros true]
+              [monkeycharts.utils :as utils]))
 
 (enable-console-print!)
 
-(println "Edits to this text should show up in your developer console.")
+(defonce app-state (atom {:school-size []}))
 
-;; define your app data so that it doesn't get over-written on reload
+(defn chart-school-size [school-size _owner]
+  (reify
+    om/IRender
+    (render [_]
+      (apply dom/div nil
+             (map (comp #(dom/p nil %)
+                        #(apply str %)
+                        #(interpose " " %)
+                        vals)
+                  school-size)))))
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defn load-it-up []
+  (utils/edn-xhr
+   {:method :get
+    :url "/query/school-size"
+    :on-error (fn [err] (println err))
+    :on-complete (fn [res] (om/update! (om/root-cursor app-state) :school-size res))}))
 
 (om/root
-  (fn [data owner]
+  (fn [app owner]
     (reify om/IRender
       (render [_]
-        (dom/h1 nil (:text data)))))
+        (om/build chart-school-size (:school-size app)))))
   app-state
   {:target (. js/document (getElementById "app"))})
 
